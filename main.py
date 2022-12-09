d40583546660fc5c0d166745aaa9285307e7cce3
cojfulfer <conjfulfer@gmail.com>
2022-12-08 18:17:21 -0600

    This is the commented outline for the project.

---
+++ main.py
@@ -0,0 +1,5 @@
+# Step 1: Prompt user to import their Goodreads "Want to Read" list sorted by greatest to least number of ratings
+# Step 2: prompt user to import their Goodreads "Want to Read" list sorted by greatest to least average rating
+# Step 3: Create a new list of the books that both lists share first
+# Step 4: Output to user their "Want to Read" list sorted by greatest to least number of ratings AND average rating
+# Step 5: update new list when user adds or removes books from their "Want to Read" list
